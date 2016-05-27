da3c0cdece16de0426153eeaaba95c0c61adbe78
Andrey Myznikov <andrey.myznikov@gmail.com>
2016-05-27 14:59:15 +0300

    Update gencert.sh helper script

--- ffms/gencert.sh
+++ ffms/gencert.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
+#
+# http://stackoverflow.com/questions/10175812/how-to-create-a-self-signed-certificate-with-openssl
+# https://habrahabr.ru/post/192446/
+# https://www.madboa.com/geek/openssl/#how-do-i-generate-a-self-signed-certificate
+#
 
 domain=localhost
 
-#openssl req -newkey rsa:2048 -nodes -keyout ${domain}.key -out ${domain}.csr
-openssl req -newkey rsa:2048 -nodes -keyout ${domain}.key -x509 -days 365 -out ${domain}.crt
+openssl req \
+  -newkey rsa:2048 -nodes -keyout ${domain}.key \
+  -x509 -days 365 -out ${domain}.crt \
+  -subj "/C=UA/ST=HOME/L=ROOM/O=MY_ORGANIZATION/CN=${domain}"
